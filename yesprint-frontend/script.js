132ae0952ac27ded3e49fe1eac0f437d653dd400
Sahil Rajput <sahilrajput03@gmail.com>
2026-05-02 18:58:36 +0530

    Refactor installation prompt handling in script.js and update title in index.html

--- yesprint-frontend/script.js
+++ yesprint-frontend/script.js
@@ -20,7 +20,7 @@ if ('serviceWorker' in navigator) {
 let isFirstEventFire_beforeinstallprompt = true;
 
 // Prompt for installation
-let installPromptRef = { current: null };
+let installPromptEvent = null;
 window.addEventListener('beforeinstallprompt', (e) => {
     e.preventDefault(); // Prevent Chrome from showing the default prompt
     // Learn: Below line throws - Uncaught (in promise)
@@ -28,7 +28,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
     //      'BeforeInstallPromptEvent': The prompt() method must be called
     //      with a user gesture
     // const result = await e.prompt();
-    installPromptRef.current = e;
+    installPromptEvent = e;
     if (isFirstEventFire_beforeinstallprompt) {
         isFirstEventFire_beforeinstallprompt = false;
         $("#modal").removeAttribute("hidden");
@@ -43,15 +43,14 @@ $('#maybe-later-btn').addEventListener('click', () => {
 });
 
 async function handleInstall() {
-    const installPrompt = installPromptRef.current;
-    if (!installPrompt) {
+    if (!installPromptEvent) {
         // alert("Install prompt not available");
         console.log("Install prompt not available");
         return;
     }
-    const result = await installPrompt.prompt();
+    const result = await installPromptEvent.prompt();
     const isAccepted = result.outcome === 'accepted';
-    installPromptRef.current = null;
+    installPromptEvent = null;
     // alert(isAccepted ? 'Accepted ✅' : 'Rejected ❌');
     console.log(`Install prompt was: ${result.outcome}`); // "accepted" or "dismissed"
 
