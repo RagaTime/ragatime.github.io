ee78c29b48debb972e65198545c1961738b7b275
Sahil Rajput <sahilrajput03@gmail.com>
2026-05-03 17:18:24 +0530

    Update.

--- lineflow-frontend/copy-to-linode.sh
+++ lineflow-frontend/copy-to-linode.sh
@@ -1,4 +1,7 @@
 # Learn: These notes added to archlinux doc (search for: "Public Files Server")
 # Learn: Below command was required for once only so that I can copy file via `scp`.
 # sudo chown -R user1: /var/www/lineflow-frontend/
-scp -r ./* linode.user1:/var/www/lineflow-frontend/
+# scp -r ./* linode.user1:/var/www/lineflow-frontend/
+
+# Only copying index.html file for releases purpose
+scp -r ./index.html linode.user1:/var/www/lineflow-frontend/
